f4a55ffa26c18588da24b6df6d6df887bba6147c
Joao Victor <jvspepe@proton.me>
2023-12-20 16:55:55 -0300

    Change MUI to TailwindCSS.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
+import { ThemeProvider } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
+import theme from "@/styles/global";
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Outlet />
-    </>
+    </ThemeProvider>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,22 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "@fontsource-variable/archivo";
 import "@fontsource-variable/plus-jakarta-sans";
+import "index.css";
 import Home from "./pages/Home.tsx";
 import App from "./App.tsx";
+import RootLayout from "./layouts/RootLayout.tsx";
 
 const browserRouter = createBrowserRouter([
-  { element: <App />, children: [{ path: "/", element: <Home /> }] },
+  {
+    element: <App />,
+    children: [
+      { element: <RootLayout />, children: [{ path: "/", element: <Home /> }] },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={browserRouter} />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
