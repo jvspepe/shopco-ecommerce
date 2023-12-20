2d76a332e2affa670714d4c42d2d4a4bfa870cad
Joao Victor <jvspepe@proton.me>
2023-12-20 18:37:26 -0300

    Remove wrong imports.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
 import { Outlet } from "react-router-dom";
-import { ThemeProvider } from "@mui/material";
-import CssBaseline from "@mui/material/CssBaseline";
-import theme from "@/styles/global";
 
 const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Outlet />
-    </ThemeProvider>
-  );
+  return <Outlet />;
 };
 
 export default App;
