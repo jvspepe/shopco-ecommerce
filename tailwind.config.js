7de3b71b43d1badabd43c2cf3d0e325cdbfc94af
Joao Victor <jvspepe@proton.me>
2023-12-20 17:24:25 -0300

    Update config and plugins.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: "'Plus Jakarta Sans Variable', sans-serif",
+        heading: "'Archivo Variable', sans-serif",
+      },
+    },
   },
   plugins: [],
 };
