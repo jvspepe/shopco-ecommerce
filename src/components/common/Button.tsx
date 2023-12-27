5a4121f77819ae3a54b120d65de28f415ebc16e2
Joao Victor <jvspepe@proton.me>
2023-12-27 20:04:20 -0300

    Fix Button component.

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -5,7 +5,7 @@ export type Props<T extends ElementType> = {
   component?: T;
 };
 
-const IconButton = <T extends ElementType = "button">({
+const Button = <T extends ElementType = "button">({
   component,
   ...props
 }: Props<T> & Omit<React.ComponentPropsWithoutRef<T>, keyof Props<T>>) => {
@@ -16,7 +16,7 @@ const IconButton = <T extends ElementType = "button">({
     <Component
       {...rest}
       className={classMerge(
-        "flex items-center justify-between gap-2 rounded p-1 font-heading uppercase transition-colors duration-300 hover:bg-black/10",
+        "flex items-center justify-center gap-3 rounded-full bg-black p-1 px-8 py-4 text-center font-heading text-white transition-colors duration-300 hover:bg-black/50",
         className,
       )}
     >
@@ -25,4 +25,4 @@ const IconButton = <T extends ElementType = "button">({
   );
 };
 
-export default IconButton;
+export default Button;
